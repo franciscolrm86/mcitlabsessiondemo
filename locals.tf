d5f0eb22e935b9ac9ed4558c05b9bba10656f356
franciscolrm86 <168039749+franciscolrm86@users.noreply.github.com>
2024-04-26 20:23:54 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,9 +1,22 @@
 locals{
   servicename="abracadabra"
   forum="abracadabramcit"
+firstname="francisco"
+lastname="rodriguez"
+city="Montreal"
 }
 output "print"{
     value = local.servicename
+}
+output "printfirstname"{
+    value = local.firstname
+}
+output "printlastname"{
+    value = local.lastname
+}
+
+output "printcity"{
+    value = local.city
 }
 output "secondprint"{
     value = local.forum
