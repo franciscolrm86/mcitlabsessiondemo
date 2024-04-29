374294f1949f8ff798938f3a620329df5905a668
franciscolrm86 <168039749+franciscolrm86@users.noreply.github.com>
2024-04-29 18:36:09 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -4,6 +4,11 @@ locals{
 firstname="francisco"
 lastname="rodriguez"
 city="Montreal"
+landmark1 = "oldport"
+landmark2 = "basilica"
+landmark3 = "montroyal"
+landmark4 = "cartierbridge"
+landmark5 = "granderoue"
 }
 output "print"{
     value = local.servicename
@@ -21,3 +26,24 @@ output "printcity"{
 output "secondprint"{
     value = local.forum
 }
+output "printlandmark1"{
+    value = local.landmark1
+}
+output "printlandmark2"{
+    value = local.landmark2
+}
+output "printlandmark3"{
+    value = local.landmark3
+}
+output "printlandmark4"{
+    value = local.landmark4
+}
+output "printlandmark5"{
+    value = local.landmark5
+}
+
+lengthlandmark1=length(local.landmark1)
+lengthlandmark2=length(local.landmark2)
+lengthlandmark3=length(local.landmark3)
+lengthlandmark4=length(local.landmark4)
+lengthlandmark5=length(local.landmark5)
