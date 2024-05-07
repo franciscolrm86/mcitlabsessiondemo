8814e45e275c60d3fe26c84728b8fe64a36e245a
franciscolrm86 <168039749+franciscolrm86@users.noreply.github.com>
2024-05-06 20:11:01 -0400

    Update 03mayhomework.tf

--- 03mayhomework.tf
+++ 03mayhomework.tf
@@ -4,7 +4,3 @@
 #3. Check if “Canada” is part of the list of countries
 #4. Merge the following two strings “I love” and “Canada  ”. Remove the extra space in the string “Canada “ and show the string as “I love Canada”
 #5. Transform the following string “upper LOWER” to “UPPER lower”
-
-variable "countries2visit"{
-default = ["Spain", "France", "Italy", "Netherlands", "Germany"]
-}
