ffa9e4355934665f135ae53abb953f3294108e6b
franciscolrm86 <168039749+franciscolrm86@users.noreply.github.com>
2024-05-08 19:23:49 -0400

    Update Kubernetes2.tf

--- Kubernetes2.tf
+++ Kubernetes2.tf
@@ -1,1 +1,12 @@
+locals{
+cluster_names=["Storm","Wolverine","Beast","Jean","Gambit"]
+}
+resource "azurerm_resource_group" "xmen97"{
+name = "xmen97"
+location = "Canada Central"
+}
+resource "azurerm_resource_group" "xmen97"{
+name = "xmen97"
+location = "Sweden"
+}
 
