b248de4401cb2f29a5e3f114912b84968b572f21
Francisco Rodriguez <168039749+franciscolrm86@users.noreply.github.com>
2024-05-08 20:40:22 -0400

    Update kubernetesnodepool.tf

--- kubernetesnodepool.tf
+++ kubernetesnodepool.tf
@@ -1,4 +1,48 @@
-resource "azurerm_kubernetes_cluster_node_pool" "kube1nodepool" {
+resource "azurerm_kubernetes_cluster_node_pool" "Storm" {
+ for_each               = azurerm_kubernetes_cluster.batchabcd
+ name                   = "${each.key}"
+ kubernetes_cluster_id  = each.value.id
+ vm_size                = "Standard_DS2_v2"
+ node_count             = 1
+
+  tags = {
+    Environment = "Production"
+  }
+}
+resource "azurerm_kubernetes_cluster_node_pool" "Wolverine" {
+ for_each               = azurerm_kubernetes_cluster.batchabcd
+ name                   = "${each.key}"
+ kubernetes_cluster_id  = each.value.id
+ vm_size                = "Standard_DS2_v2"
+ node_count             = 1
+
+  tags = {
+    Environment = "Production"
+  }
+}
+resource "azurerm_kubernetes_cluster_node_pool" "Rogue" {
+ for_each               = azurerm_kubernetes_cluster.batchabcd
+ name                   = "${each.key}"
+ kubernetes_cluster_id  = each.value.id
+ vm_size                = "Standard_DS2_v2"
+ node_count             = 1
+
+  tags = {
+    Environment = "Production"
+  }
+}
+resource "azurerm_kubernetes_cluster_node_pool" "Jean" {
+ for_each               = azurerm_kubernetes_cluster.batchabcd
+ name                   = "${each.key}"
+ kubernetes_cluster_id  = each.value.id
+ vm_size                = "Standard_DS2_v2"
+ node_count             = 1
+
+  tags = {
+    Environment = "Production"
+  }
+}
+resource "azurerm_kubernetes_cluster_node_pool" "Gambit" {
  for_each               = azurerm_kubernetes_cluster.batchabcd
  name                   = "${each.key}"
  kubernetes_cluster_id  = each.value.id
